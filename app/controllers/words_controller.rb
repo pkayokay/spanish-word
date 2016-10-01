792a6afc3d96cb359e9eb6093209d17a992cea1e
Paul <pkayokay@gmail.com>
2016-10-01 03:33:52 +0000

    width of box

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -2,4 +2,8 @@ class WordsController < ApplicationController
   def index
       @word = Word.order("RANDOM()").last
   end
+
+  def audio_path(source, options = {})
+    path_to_asset(source, {type: :audio}.merge!(options))
+  end
 end
